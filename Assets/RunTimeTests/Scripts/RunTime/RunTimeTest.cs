aea435861ee6f4ed5d94756fc07d66f662f7cbe6
IShix <49128115+IShix-g@users.noreply.github.com>
2025-11-22 15:29:57 +0900

    update test data

--- Assets/RunTimeTests/Scripts/RunTime/RunTimeTest.cs
+++ Assets/RunTimeTests/Scripts/RunTime/RunTimeTest.cs
@@ -8,26 +8,33 @@ namespace Tests
     public class RunTimeTest : MonoBehaviour
     {
         [SerializeField] Button _dialogButton;
-        
+        [SerializeField] Button _saveButton;
+
         TestModelRepository _repository;
-        
+
         public void Inject(TestModelRepository repository) => _repository = repository;
 
         // Initialize the repository that implements IHistSaveDataHandler in Awake
         void Awake() => Hist.Configure(_repository);
 
-        void Start() => _dialogButton.onClick.AddListener(OnDialogButtonClicked);
-        
+        void Start()
+        {
+            _dialogButton.onClick.AddListener(OnDialogButtonClicked);
+            _saveButton.onClick.AddListener(OnSaveButtonClicked);
+        }
+
         void OnDialogButtonClicked()
         {
             // Display a dialog by creating an object and calling OpenDialog.
-            var obj = Hist.CreateOrGet();
+            var obj = Hist.CreateOrGetUI();
             obj.OpenDialog(() =>
             {
                 // You can also release it when the dialog closes.
                 // Hist.Release();
             });
         }
+
+        void OnSaveButtonClicked() => Hist.SaveHistory();
     }
 }
 #endif

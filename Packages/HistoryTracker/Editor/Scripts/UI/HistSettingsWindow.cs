52a67978cb0bdb871ddc9680a184e9d913a8588b
IShix <49128115+IShix-g@users.noreply.github.com>
2025-11-25 17:08:15 +0900

    refactor: reorder initialization and set default value

--- Packages/HistoryTracker/Editor/Scripts/UI/HistSettingsWindow.cs
+++ Packages/HistoryTracker/Editor/Scripts/UI/HistSettingsWindow.cs
@@ -28,7 +28,7 @@ namespace HistoryTracker.Editor
         GUIContent _updateIcon;
         GUIContent _helpHeader;
         bool _hasNewVersion;
-        CancellationTokenSource _tokenSource;
+        CancellationTokenSource _tokenSource = new ();
 
         void OnEnable()
         {
